1102a612c39b4878d4a5e3ed8a4233a4a7dfdb1f
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-04-02 19:16:53 +0545

    testing husky

--- async-functions/eslint.config.mjs
+++ async-functions/eslint.config.mjs
@@ -7,8 +7,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"],
-  rules:{
-    "no-unused-vars":"error"
-  } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"]},
+  {
+    rules: {
+			"no-unused-vars": "error"
+		},
+  },
 ]);
