33efb5774c8372c5eb9754aa258e81c4f45e417a
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-04-03 12:47:35 +0545

    testing husky

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -5,3 +5,12 @@ var a=2
 var b=5
 var sum=a+b
 console.log(sum)
+
+function example(arg=1){
+    if(arg==1){
+        //dom something
+    }
+    else if(arg==2){
+        //do something else
+    }
+}
