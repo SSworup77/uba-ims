c28452509abf53314177ddb31f248564f949465e
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-04-06 21:49:17 +0545

    Learned about promises

--- asynchronous/promise/promise-1.js
+++ asynchronous/promise/promise-1.js
@@ -1,6 +1,10 @@
-const a = 3;
-const b = 4;
-const sum = (a, b) => {
-    return a + b;
+function divide(num1, num2){
+    return new Promise((resolve,reject)=>{
+        if(num2===0){
+            reject("Cannot divide by zero")
+        }else{
+            resolve(num1/num2)
+        }
+    })
 }
-console.log(`The sum of ${a} and ${b} is ${sum(a, b)}`);
+divide(10,0).then((result)=>console.log(result)).catch((error)=>console.log(error))
